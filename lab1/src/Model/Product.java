49cdbe3116fd884ef0576e7fdf5dc0ad8cdea983
sameeksha234 <bindiganavilesriva.s@northeastern.edu>
2025-09-24 19:35:59 -0400

    lab 1 completed

--- lab1/src/Model/Product.java
+++ lab1/src/Model/Product.java
@@ -9,5 +9,41 @@ package Model;
  * @author samee
  */
 public class Product {
+    String name;
+    String description;
+    String availNum;
+    String price;
+    Address manufactureAddress;   
+    Address shippingAddress;
+   
+    
+    public Product(){
+        manufactureAddress = new Address();
+        shippingAddress = new Address();
+    }
+
+    public void setName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setDescription(String description) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setAvailNum(String avail) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setPrice(String price) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void getManufactureAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
